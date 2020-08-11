ad0892b2f68f9c3757c03a2328664271e82db35f
sigismundo4321 <sigismundo4321@gmail.com>
2020-08-10 21:09:55 -0300

    inicio  na view de detalhe

---
+++ cook/lib/views/meal_detail_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MealDetailView extends StatelessWidget {
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalhes da Refeição"),
+        centerTitle: true,
+      ),
+
+
+      body: Center(),
+      
+    );
+  }
+}
